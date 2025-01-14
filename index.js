bd0168bdd262a82523eef91374d23079fc004e53
ashu <ashu00665@gmail.com>
2025-01-14 21:13:45 +0530

    Initial commit added

---
+++ index.js
@@ -0,0 +1,17 @@
+const age=21
+const name='Ashutosh Gaurav'
+const dob='01/01/2002'
+const g='male'
+
+function sayName() {
+    console.log('my name is d ');
+}
+function sayNamee() {
+    console.log('my name is d ');
+}
+function sayNamede() {
+    console.log('my name is d ');
+}
+function saedyNamede() {
+    console.log('my name is d ');
+}
